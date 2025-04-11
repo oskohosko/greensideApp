cee8da31f8960ad3605b497bb4967b932a42f5e4
Oskar Hosken <oskarhosken@gmail.com>
2025-04-11 21:24:30 +1000

    Updated gitignore

---
+++ Greenside/Greenside/Views/Main/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Greenside
+//
+//  Created by Oskar Hosken on 11/4/2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
